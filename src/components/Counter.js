83efa38dfafb74541a579f99b3b08d57cdf30b4f
tadams95 <tyrelleadams28@gmail.com>
2022-12-16 18:07:13 -0800

    Toggle Counter Div

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,27 +5,29 @@ import React from "react";
 
 const Counter = () => {
   const dispatch = useDispatch();
-
   const counter = useSelector((state) => state.counter);
+  const show = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
 
   const increaseHandler = () => {
-    dispatch({type: "increase", amount: 5})
-  }
+    dispatch({ type: "increase", amount: 5 });
+  };
 
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}> {counter} </div>
+      {show && <div className={classes.value}> {counter} </div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
@@ -39,7 +41,6 @@ const Counter = () => {
 export default Counter;
 
 // class Counter extends Component {
-
 
 //   incrementHandler(){
 //     this.props.increment();
@@ -79,7 +80,7 @@ export default Counter;
 //       increment: () => dispatch({type: "increment"}),
 //       decrement: () => dispatch({type: "decrement"})
 
-//     } 
+//     }
 //   }
 
 // export default connect(mapStateToProps, mapDispatchToProps) (Counter);
